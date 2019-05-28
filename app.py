21bd3249155bf71234fd6f4d344c8da9e88aa3e3
Joseph M. O'Connor <tiluser0@gmail.com>
2019-05-27 20:02:45 -0400

    Fix multiply_numbers

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def multiply_numbers_post():
 	  # --> ['5', '6', '8']
 	  # print(type(request.form['text']))
 	  if request.method == 'GET':
-	  	return render_template('add_numbers.html')
+	  	return render_template('multiply_numbers.html')
 	  elif request.method == 'POST':
   	      print(request.form['text'].split())
   	      total = 0
